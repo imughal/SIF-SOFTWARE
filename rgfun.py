9a857bea8cb7e81e52a119098d8ecf585c9ee260
imughal <mughalscomputers@gmail.com>
2015-04-01 09:51:18 +0400

    updated rgfun
    Regular Functions updated

---
+++ rgfun.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+def br():
+	print ""
+	
+def stline():
+	print "------------------------"
+
+def waits():
+	try:
+		n = 1
+		while n<(9999 * 999):
+			n += 1
+	except:
+		print "print Error"
+
+def get_int(message,x=""):
+	while True:
+		try:
+			new_int=int(raw_input(message))
+			break
+		except:
+			print "Invalid Input"
+	return new_int
+
+def check_inp(inp,x):
+	if x = "Year":
+		
+	
+
+
+if __name__ == "__main__":
+	print "Error-Invalid File to Run- Please Run main.py."
