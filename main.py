24ecb1469ede96a02e3a468713179ae20220df74
imughal <mughalscomputers@gmail.com>
2015-04-06 21:09:22 +0400

    changing

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
-#Importing
+# Importing
 import xmlClass
 from rgfun import *
 
-#new
+# new
+
+
 newPrint = xmlClass.xmlDataBase("employees.xml")
 empData = newPrint.dataImp()
 for i in empData:
-	newa = [i.name,i.iban,i.basic,i.lcard,i.company,i.brno,i.cocode]
-	print newa[2]
-	br()
+    newa = [i.name, i.iban, i.basic, i.lcard, i.company, i.brno, i.cocode]
+    print newa[2]
+    br()
